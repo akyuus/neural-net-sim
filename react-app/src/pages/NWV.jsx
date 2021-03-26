6f9a36af6c88ec4de6d020435b29624cc03df40c
jph6366 <42678151+jph6366@users.noreply.github.com>
2021-03-26 15:40:21 -0400

    Implement pages and components

---
+++ react-app/src/pages/NWV.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NWV = () => {
+
+    return (
+        <>
+            <h1>Neural Weight Virtualization</h1>
+            <p>Create NWV on this page. Create components as needed. </p>
+        </>
+    )
+}
+
+export default Jackson; 
---
+++ react-app/src/pages/SubFlow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SubFlow = () => {
+
+    return (
+        <>
+            <h1>SubFlow</h1>
+            <p>Create SubFlow on this page. Create components as needed.p</p>
+        </>
+    )
+}
+
+export default Jackson; 
