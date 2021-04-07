15430d1b86414360e62f9184198044b0ee981c5b
Spencer <sjones97@email.unc.edu>
2021-04-07 12:45:15 -0400

    add dummy graphs and reformat nwv ui

--- typescript-app/src/components/NeuralWeightUI.tsx
+++ typescript-app/src/components/NeuralWeightUI.tsx
@@ -1,16 +1,98 @@
 import "../styles.css";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ComposedChart,
+    Legend,
+    Bar
+  } from "recharts";
 
-// import {
-//     LineChart,
-//     Line,
-//     XAxis,
-//     YAxis,
-//     CartesianGrid,
-//     Tooltip,
-//     ComposedChart,
-//     Legend,
-//     Bar
-//   } from "recharts";
+  const data = [{
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
+
+const data2 = [
+  {
+    name: 'Page A',
+    uv: 590,
+    pv: 800,
+    amt: 1400,
+  },
+  {
+    name: 'Page B',
+    uv: 868,
+    pv: 967,
+    amt: 1506,
+  },
+  {
+    name: 'Page C',
+    uv: 1397,
+    pv: 1098,
+    amt: 989,
+  },
+  {
+    name: 'Page D',
+    uv: 1480,
+    pv: 1200,
+    amt: 1228,
+  },
+  {
+    name: 'Page E',
+    uv: 1520,
+    pv: 1108,
+    amt: 1100,
+  },
+  {
+    name: 'Page F',
+    uv: 1400,
+    pv: 680,
+    amt: 1700,
+  },
+];
 
 export default function NeuralWeightUI() {
     return (
@@ -41,21 +123,78 @@ export default function NeuralWeightUI() {
                         <div className="column">
                             <div className="row">
                                 <label className="checkbox">
-                                    <input type="checkbox" />
+                                    <input type="checkbox" />{" "}
                                     ResNet
                                 </label>
                             </div>
                             <div className="row">
                                 <label className="checkbox">
-                                    <input type="checkbox" />
+                                    <input type="checkbox" />{" "}
                                     IM2TXT
                                 </label>
                             </div>
                             <div className="row">
                                 <label className="checkbox">
-                                    <input type="checkbox" />
+                                    <input type="checkbox" />{" "}
                                     Inception
                                 </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="subtitle is-2">Graph 1</h2>
+                            <div className="box">
+                                <LineChart
+                                width={400}
+                                height={400}
+                                data={data}
+                                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                                >
+                                <XAxis dataKey="name" />
+                                <Tooltip />
+                                <CartesianGrid stroke="#f5f5f5" />
+                                <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+                                <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+                                </LineChart>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="subtitle is-2">Graph 2</h2>
+                            <div className="box">
+                                <LineChart
+                                width={400}
+                                height={400}
+                                data={data}
+                                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                                >
+                                <XAxis dataKey="name" />
+                                <Tooltip />
+                                <CartesianGrid stroke="#f5f5f5" />
+                                <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+                                <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+                                </LineChart>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="subtitle is-2">Graph 3</h2>
+                            <div className="box">
+                                <LineChart
+                                width={400}
+                                height={400}
+                                data={data}
+                                margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                                >
+                                <XAxis dataKey="name" />
+                                <Tooltip />
+                                <CartesianGrid stroke="#f5f5f5" />
+                                <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+                                <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+                                </LineChart>
                             </div>
                         </div>
                     </div>
