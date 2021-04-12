952757828c82852a17478e3120d04b2a90359dbe
Spencer <sjones97@email.unc.edu>
2021-04-12 16:19:38 -0400

    fix running errors

--- typescript-app/src/components/NeuralWeightUI.tsx
+++ typescript-app/src/components/NeuralWeightUI.tsx
@@ -3,12 +3,8 @@ import {
     LineChart,
     Line,
     XAxis,
-    YAxis,
     CartesianGrid,
-    Tooltip,
-    ComposedChart,
-    Legend,
-    Bar
+    Tooltip
   } from "recharts";
 
   const data = [{
@@ -53,45 +49,6 @@ import {
     pv: 4300,
     amt: 2100
   }
-];
-
-const data2 = [
-  {
-    name: 'Page A',
-    uv: 590,
-    pv: 800,
-    amt: 1400,
-  },
-  {
-    name: 'Page B',
-    uv: 868,
-    pv: 967,
-    amt: 1506,
-  },
-  {
-    name: 'Page C',
-    uv: 1397,
-    pv: 1098,
-    amt: 989,
-  },
-  {
-    name: 'Page D',
-    uv: 1480,
-    pv: 1200,
-    amt: 1228,
-  },
-  {
-    name: 'Page E',
-    uv: 1520,
-    pv: 1108,
-    amt: 1100,
-  },
-  {
-    name: 'Page F',
-    uv: 1400,
-    pv: 680,
-    amt: 1700,
-  },
 ];
 
 export default function NeuralWeightUI() {
