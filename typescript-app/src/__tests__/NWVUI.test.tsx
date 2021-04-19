7bf43acc189a6450536715c83ee61a1123933d14
jph6366 <42678151+jph6366@users.noreply.github.com>
2021-04-19 11:30:22 -0400

    jest tests for two components

---
+++ typescript-app/src/__tests__/NWVUI.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import NeuralWeightUI from "../components/NeuralWeightUI";
+
+describe("<NeuralWeightUI />", () => {
+  test("should display working SubFlow charts", async () => {
+    // ???
+  });
+});
