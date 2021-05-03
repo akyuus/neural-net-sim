be430dedcd49d16e03e0889d3f9c346a5ad01f21
Spencer <sjones97@email.unc.edu>
2021-05-03 00:33:24 -0400

    fix deployment and add testing

--- typescript-app/src/components/Navigation.tsx
+++ typescript-app/src/components/Navigation.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+// import { Typography } from '@material-ui/core';
 import { Nav, Navbar } from "react-bootstrap";
 import Logo from "../icons/logo.svg";
 declare module "*.svg";
@@ -11,8 +13,10 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/subflow">Subflow</Nav.Link>
-                    <Nav.Link href="/neuralweightvirtualization">Neural Weight Virtualization</Nav.Link>
+                    {/* <Typography component={Link} to="/subflow">Subflow</Typography>
+                    <Typography component={Link} to="/neuralweightvirtualization">Neural Weight Virtualization</Typography> */}
+                    <Nav.Link as={Link} to="/subflow">Subflow</Nav.Link>
+                    <Nav.Link as={Link} to="/neuralweightvirtualization">Neural Weight Virtualization</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Navbar>
