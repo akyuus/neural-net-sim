7f05fc0abf165b62505af002f0d9f6f5783818a4
Spencer <sjones97@email.unc.edu>
2021-05-03 00:36:25 -0400

    fix home page

--- typescript-app/src/pages/Home.tsx
+++ typescript-app/src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         }}>
             <Navigation />
             <h1 className="display-3">Welcome!</h1>
-            <p className="lead">This project was created by <a href="https://tarheels.live/arsj523/">COMP 523 Team J</a></p>
+            <p className="lead">This project was implemented by <a href="https://tarheels.live/arsj523/">COMP 523 Team J</a></p>
         </div>
     )
 }
